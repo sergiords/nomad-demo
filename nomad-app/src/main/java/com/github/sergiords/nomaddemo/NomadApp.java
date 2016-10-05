c9f2f9d94d64057ac79d4f87463f7fea298c82c0
Sergio Dos Santos <sergiords@users.noreply.github.com>
2016-10-05 22:23:13 +0200

    move configuration into application

--- nomad-app/src/main/java/com/github/sergiords/nomaddemo/NomadApp.java
+++ nomad-app/src/main/java/com/github/sergiords/nomaddemo/NomadApp.java
@@ -1,7 +1,9 @@
 package com.github.sergiords.nomaddemo;
 
+import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NomadApp {
@@ -10,4 +12,10 @@ public class NomadApp {
         SpringApplication.run(NomadApp.class, args);
     }
 
+    @Bean
+    public ResourceConfig getResourceConfig() {
+        ResourceConfig resourceConfig = new ResourceConfig();
+        resourceConfig.register(NomadResource.class);
+        return resourceConfig;
+    }
 }
