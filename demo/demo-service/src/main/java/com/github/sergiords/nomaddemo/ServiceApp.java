74da76f93cc95402a0d8a399cecc78bff9cb37f7
Sergio Dos Santos <sergiords@users.noreply.github.com>
2016-11-01 00:35:35 +0100

    add version to hello service and support missing properties and environment variables

--- demo/demo-service/src/main/java/com/github/sergiords/nomaddemo/ServiceApp.java
+++ demo/demo-service/src/main/java/com/github/sergiords/nomaddemo/ServiceApp.java
@@ -3,20 +3,27 @@ package com.github.sergiords.nomaddemo;
 import net.codestory.http.WebServer;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class ServiceApp {
 
     public static void main(String[] args) throws IOException {
 
-        String nodeName = System.getProperty("node.name");
-        String nodeIndex = System.getenv("NOMAD_ALLOC_INDEX");
-        Integer httpPort = Integer.valueOf(System.getenv("NOMAD_PORT_http"));
+        String nodeName = System.getProperty("node.name", "N/A");
+        String nodeIndex = Optional.ofNullable(System.getenv("NOMAD_ALLOC_INDEX"))
+                .orElse("N/A");
+        Integer httpPort = Optional.ofNullable(System.getenv("NOMAD_PORT_http"))
+                .map(Integer::valueOf)
+                .orElse(8080);
+        String version = Optional.ofNullable(ServiceApp.class.getPackage().getImplementationVersion())
+                .orElse("N/A");
 
         new WebServer().configure(routes -> routes.get("/hello", (context) -> {
 
             System.out.println("Receiving request from " + context.query().get("from"));
             System.out.println("Sending response from " + nodeName + "#" + nodeIndex);
-            return nodeName + "#" + nodeIndex;
+
+            return "Node: " + nodeName + " - Instance: #" + nodeIndex + " - Version: " + version;
 
         })).start(httpPort);
     }
